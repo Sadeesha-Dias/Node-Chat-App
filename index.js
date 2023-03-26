54b76f7f3d142ce869209c1f504b20933f0c7cc0
Sadeesha-Dias <sddias19981230@gmail.com>
2023-03-26 19:53:09 +0530

    Issue Fixed: displaying server side incoming message twice in client side.

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
+const readline = require('readline');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
+
+//creating a readline interface to read input from the terminal
+const serverInput = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 app.use(express.static(__dirname + '/public'));
 
@@ -18,19 +25,34 @@ io.on('connection', (socket) => {
     // Send a welcome message to the newly connected client
     socket.emit('chatMessage', 'Welcome to the chat app!');
 
+    // socket.on('chatMessage', (message) => {
+    //     io.emit('message', message);
+
+    //     if(message.startsWith('Server: ')) {
+    //         socket.emit('chatMessage', message);
+    //     }else{
+    //         socket.broadcast.emit('chatMessage', message);
+    //     }
+    // });
+
+    
     // Listen for incoming messages from the client
     socket.on('chatMessage', (message) => {
-        console.log('Client-message: ' + message);
+        console.log('Client: ' + message);
 
         // Broadcast the message to all connected clients
-        io.emit('chatMessage', message);
+        io.emit('chatMessage', 'Client: ' + message);
+    });
+
+    //Listen for input from the terminal (server side)
+    serverInput.on('line', (input) => {
+        //emit the input to all the connected clients
+        socket.broadcast.emit('chatMessage', 'Server: ' + input)
     });
 
     socket.on('disconnect', () => {
         console.log('A user has disconnected!');
     });
 
-    // socket.on('chatMessage', (message) => {
-    //     io.emit('message', message);
-    // });
+    
 });
