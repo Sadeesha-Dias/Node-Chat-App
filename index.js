c94b39c51787ea7d8c9497a1c10211b66e4267e1
Sadeesha-Dias <sddias19981230@gmail.com>
2023-03-26 14:08:17 +0530

    imported required modules and created server and port to listen

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static(__dirname + '/public'));
+
+server.listen(4050, () => {
+    console.log('Server is up and running on port 4050');
+});
